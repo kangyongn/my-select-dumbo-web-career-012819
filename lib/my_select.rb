c3b07edef8ee3653f5adfe242980f8adb87881cb
Yong Nam Kang <j.yongnamkang@gmail.com>
2019-01-12 06:05:15 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,10 @@
 def my_select(collection)
   i = 0
+  element = ""
   new_collection = []
   
   while i < collection.length
     yield collection[i]
-    new_collection << collection[i]
-    end
     i += 1
   end
   
